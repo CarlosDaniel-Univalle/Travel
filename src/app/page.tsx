aa46150697d363e11647dba6513425b0d3234150
andres <andresferrufinoj@gmail.com>
2023-11-01 08:20:15 -0400

    formulario actualizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import Image from 'next/image';
 import { FaGoogle, FaFacebookF } from 'react-icons/fa';
 import Link from 'next/link';
+//import app from './firebaseconfig';
+
+import { initializeApp, getApps, getApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCoC0K7qGLi2g3drg4LuML6jf2KZRpqR2U",
+    authDomain: "login-56f35.firebaseapp.com",
+    projectId: "login-56f35",
+    storageBucket: "login-56f35.appspot.com",
+    messagingSenderId: "372955814414",
+    appId: "1:372955814414:web:506130c01bdbd7176f3a5f",
+    measurementId: "G-YHBTP3ENNF"
+  };
+
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export default function LoginForm() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        const auth = getAuth();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+
+            console.log("buen dia");
+            
+        } catch (error) {
+            
+            console.error(error);
+            console.log(firebaseConfig)
+        }
+    };
+
     return (
         <div className="flex h-screen">
             <div className="w-1/2 h-full relative">
@@ -13,21 +49,21 @@ export default function LoginForm() {
             <div className="w-1/2 h-full flex flex-col justify-center items-center bg-white p-8">
                 <h1 className="text-2xl font-semibold mb-4">¡Bienvenido a un mundo de viajes sin límites!</h1>
                 
-                <form className="w-4/5 flex flex-col">
+                <form className="w-4/5 flex flex-col" onSubmit={handleSubmit}>
                     <label className="mb-2">Correo electrónico</label>
-                    <input type="email" className="border p-2 rounded mb-4" required />
+                    <input type="email" className="border p-2 rounded mb-4" required value={email} onChange={(e) => setEmail(e.target.value)} />
                     
                     <label className="mb-2">Contraseña</label>
-                    <input type="password" className="border p-2 rounded mb-4" required />
+                    <input type="password" className="border p-2 rounded mb-4" required value={password} onChange={(e) => setPassword(e.target.value)} />
                     
                     <button type="submit" className="bg-blue-500 text-white p-2 rounded mb-4">Iniciar sesión</button>
                     
                     {/* Actualización para el uso de Link sin el elemento a */}
                     <Link href="/RegisterPage" passHref>
-                    <button type="button" className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-800 transition duration-200 w-full text-center">
-                        Registrarme
-                    </button>
-                </Link>
+                        <button type="button" className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-800 transition duration-200 w-full text-center">
+                            Registrarme
+                        </button>
+                    </Link>
                 </form>
                 
                 <div className="flex items-center mb-4">
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,41 +1,88 @@
-// components/LoginForm.js
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const LoginForm = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aquí puedes enviar los datos del formulario al servidor para el inicio de sesión
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAI7a9MceVcCz24wJTcyOtEtNdr0x4I1f8",
+  authDomain: "travellogin-ce516.firebaseapp.com",
+  projectId: "travellogin-ce516",
+  storageBucket: "travellogin-ce516.appspot.com",
+  messagingSenderId: "1074019852700",
+  appId: "1:1074019852700:web:a479f360bc46780a506859",
+  measurementId: "G-3XCDG3E2TQ"
+};
+
+if (!getApps().length) {
+  initializeApp(firebaseConfig);
+}
+
+export default function RegisterForm() {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const [message, setMessage] = useState(''); // Mensaje para el usuario
+
+  const onSubmit = async (data) => {
+    try {
+      const auth = getAuth();
+      await createUserWithEmailAndPassword(auth, data.email, data.password);
+      setMessage('Usuario registrado con éxito!');
+      reset();
+    } catch (error) {
+      setMessage(error.message);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        placeholder="Correo electrónico"
-      />
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-        placeholder="Contraseña"
-      />
-      <button type="submit">Iniciar sesión</button>
-    </form>
-  );
-};
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="card p-5 shadow-sm" style={{ maxWidth: '400px', width: '100%' }}>
+        <h2 className="text-center mb-4">Registro de Usuario</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="username">Nombre de usuario</label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              {...register('username', { required: 'Este campo es requerido' })}
+              required
+            />
+            {errors.username && <p className="text-danger">{errors.username.message}</p>}
+          </div>
 
-export default LoginForm;
+          <div className="mb-3">
+            <label className="form-label" htmlFor="email">Correo electrónico</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              {...register('email', { required: 'Este campo es requerido' })}
+              required
+            />
+            {errors.email && <p className="text-danger">{errors.email.message}</p>}
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label" htmlFor="password">Contraseña</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              {...register('password', { required: 'Este campo es requerido', minLength: { value: 6, message: 'La contraseña debe tener al menos 6 caracteres' }})}
+              required
+            />
+            {errors.password && <p className="text-danger">{errors.password.message}</p>}
+          </div>
+
+          <button type="submit" className="btn btn-primary w-100">
+            Registrarse
+          </button>
+        </form>
+        {message && <div className="mt-3 text-center text-danger">{message}</div>} {/* Aquí se muestra el mensaje */}
+      </div>
+    </div>
+  );
+}
